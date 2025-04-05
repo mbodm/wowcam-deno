2bf4f6fda580b4744d780c145121971be3babfcb
MBODM <19464647+mbodm@users.noreply.github.com>
2025-04-05 11:57:12 +0200

    Testing with browserless.io

--- puppeteer.ts
+++ puppeteer.ts
@@ -2,7 +2,14 @@ import puppeteer from "npm:puppeteer";
 
 export async function run() {
     const url = "http://www.curseforge.com/wow/addons/raiderio";
-    const browser = await puppeteer.launch();
+
+    //const browser = await puppeteer.launch();
+    const token = "S42yoRC63FDa8Q934df9427c3a356b624f3ab12320";
+    //const launchArgs = JSON.stringify({ stealth: true });
+    //const endpoint = `wss://production-sfo.browserless.io/?token=${token}&proxy=residential&launch=${launchArgs}`;
+    const endpoint = `wss://production-sfo.browserless.io/?token=${token}&proxy=residential`;
+    const browser = await puppeteer.connect({ browserWSEndpoint: endpoint });
+
     const page = await browser.newPage();
     await page.setUserAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36");
     await page.goto(url, {
