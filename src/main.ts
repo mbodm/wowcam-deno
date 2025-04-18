2c45a6afee79284c7e23e4c156c8a48b5233e6fb
MBODM <webmaster@mbodm.com>
2025-04-18 13:05:29 +0200

    Fixed token (had chars in it, which are not allowed in URL queries). Added --unstable-cron to use Deno Cron stuff. Changed 30min crontab to 1min, for testing.

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { serve } from "./server.ts";
 import { update } from "./logic.ts";
 
 if (import.meta.main) {
-  Deno.cron("Scrape Curse", "*/30 * * * *", async () => {
+  Deno.cron("Scrape Curse", "*/1 * * * *", async () => {
     await update();
     console.log("Finished scraper cron job.");
   });
