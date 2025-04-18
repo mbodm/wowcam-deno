a9d4947b22e7b2b8721273cc2210f49c6ea56690
MBODM <webmaster@mbodm.com>
2025-04-18 12:45:39 +0200

    Removed old stuff (archive).

--- archive/browserlocal.ts
+++
@@ -1,8 +0,0 @@
-import puppeteer, { Browser } from "npm:puppeteer";
-import * as logger from "./logger.ts";
-
-export async function getBrowser(): Promise<Browser> {
-    const browser = await puppeteer.launch({ headless: false });
-    logger.log('using local puppeteer browser', true);
-    return browser;
-}
