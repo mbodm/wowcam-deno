44a3296036c38c767d14ccf34cc5eed0854bdc19
MBODM <webmaster@mbodm.com>
2025-04-25 13:12:30 +0200

    Added env variable

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,16 @@ import { serve } from "./server.ts";
 
 if (import.meta.main) {
   addOrUpdateConfig("9ccb0654-508d-4882-9983-4a7dd35e2243", ["tomtom"]); // To have something in DB
-  //Deno.cron("CronJob1", "*/1 * * * *", () => update().then(() => console.log("CRON job finished and scrapes updated.")));
-  const updateUrl = "https://mbodm-wowcam.deno.dev/scrapes/update?token=d19f023f-bfe0-437a-9daf-7ef28386ebe2";
-  Deno.cron("CronJob1", "*/1 * * * *", () => fetch(updateUrl).then(() => console.log("CRON job finished and scrapes updated.")));
+  Deno.cron("CronJob1", "*/1 * * * *", () => {
+    if (Deno.env.get("STOP") === "1") {
+      return;
+    }
+
+    //update().then(() => console.log("CRON job finished and scrapes updated."));
+
+    // Try it, by calling the endpoint from the "outside" (since this works in browser):
+    const updateUrl = "https://mbodm-wowcam.deno.dev/scrapes/update?token=d19f023f-bfe0-437a-9daf-7ef28386ebe2";
+    fetch(updateUrl).then(() => console.log("CRON job finished and scrapes updated."));
+  });
   serve();
 }
