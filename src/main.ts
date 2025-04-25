52bfd461ab6fef8310c4d295212a4c7192f016b4
MBODM <webmaster@mbodm.com>
2025-04-25 12:43:48 +0200

    Dev...

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { addOrUpdateConfig } from "./data.ts";
-import { update } from "./logic.ts";
+//import { update } from "./logic.ts";
 import { serve } from "./server.ts";
 
 if (import.meta.main) {
   addOrUpdateConfig("9ccb0654-508d-4882-9983-4a7dd35e2243", ["tomtom"]); // To have something in DB
-  Deno.cron("Scrape Curse", "*/1 * * * *", () => update().then(() => console.log("CRON job finished and scrapes updated.")));
+  //Deno.cron("CronJob1", "*/1 * * * *", () => update().then(() => console.log("CRON job finished and scrapes updated.")));
+  const updateUrl = "https://mbodm-wowcam.deno.dev/scrapes/update?token=d19f023f-bfe0-437a-9daf-7ef28386ebe2";
+  Deno.cron("CronJob1", "*/1 * * * *", () => fetch(updateUrl).then(() => console.log("CRON job finished and scrapes updated.")));
   serve();
 }
