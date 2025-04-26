9399a3c65bf09ac5e77c2ccd7bd1ac173cf224f8
MBODM <webmaster@mbodm.com>
2025-04-27 00:51:28 +0200

    Dev...

--- src/main.ts
+++ src/main.ts
@@ -13,13 +13,16 @@ if (import.meta.main) {
 
     // Try it, by calling the endpoint from the "outside" (since this works in browser):
     
-    //const updateUrl = "https://mbodm-wowcam.deno.dev/scrapes/update?token=d19f023f-bfe0-437a-9daf-7ef28386ebe2";
-    //fetch(updateUrl).then(() => console.log("CRON job finished and scrapes updated."));
+    const updateUrl = "https://mbodm-wowcam.deno.dev/scrapes/update?token=d19f023f-bfe0-437a-9daf-7ef28386ebe2";
+    const response = await fetch(updateUrl);
+    console.log("CRON job finished and scrapes updated.");
+    const text = await response.text();
+    console.log(text);
 
-    const response = await fetch("http://google.com");
-    const html = await response.text();
-    const part = html.substring(0, 50);
-    console.log(part);
+    // const response = await fetch("http://google.com");
+    // const html = await response.text();
+    // const part = html.substring(0, 50);
+    //console.log(part);
   });
   serve();
 }
