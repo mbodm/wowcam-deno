24eab2736a0b47a195e50fce69a22bebf457fbe3
MBODM <webmaster@mbodm.com>
2025-05-04 15:25:28 +0200

    name change

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import * as server from "./api/server.ts";
 import * as helper from "./core/helper.ts";
 
 if (import.meta.main) {
-  const defaultConfigName = "9ccb0654-508d-4882-9983-4a7dd35e2243";
+  const defaultConfigName = "default-9ccb0654-508d-4882-9983-4a7dd35e2243";
   dbconfigs.createOrUpdate(defaultConfigName, ["tomtom"]); // To have something in DB
   helper.log(`Created default '${defaultConfigName}' config.`);
   Deno.cron("CronJob1", "*/1 * * * *", async () => {
