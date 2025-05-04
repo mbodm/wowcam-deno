ace3223db2abb356d9be24bc2360fc07c33bba92
MBODM <webmaster@mbodm.com>
2025-05-04 15:38:51 +0200

    Tiny startup changes.

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,7 @@ import * as server from "./api/server.ts";
 import * as helper from "./core/helper.ts";
 
 if (import.meta.main) {
-  const defaultConfigName = "default-9ccb0654-508d-4882-9983-4a7dd35e2243";
-  dbconfigs.createOrUpdate(defaultConfigName, ["tomtom"]); // To have something in DB
-  helper.log(`Created '${defaultConfigName}' config.`);
+  await addDefaultConfig();
   Deno.cron("CronJob1", "*/1 * * * *", async () => {
     if (Deno.env.get("STOP") === "1") {
       return;
@@ -15,4 +13,12 @@ if (import.meta.main) {
   });
   server.serve();
   helper.log("Started Deno API server.");
+}
+
+async function addDefaultConfig() {
+  const defaultConfigName = "default-9ccb0654-508d-4882-9983-4a7dd35e2243";
+  const addons = ["tomtom"];
+  dbconfigs.createOrUpdate(defaultConfigName, addons); // To have something in DB
+  await logic.addToScrapesIfNotExisting(addons);
+  helper.log(`Added '${defaultConfigName}' config.`);
 }
