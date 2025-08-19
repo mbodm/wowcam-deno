34e1df2a79ccdd403ef29fb4712451e6de90de87
MBODM <webmaster@mbodm.com>
2025-08-19 14:21:49 +0200

    Removed Deno Cron from project (since this is now done via GitHub Actions)

--- src/main.ts
+++ src/main.ts
@@ -4,17 +4,6 @@ import * as storage from "./common/storage.ts";
 
 if (import.meta.main) {
   await addDefaultAddon();
-  Deno.cron("CronJob1", "*/30 * * * *", async () => {
-    if (Deno.env.get("STOP") === "1") {
-      return;
-    }
-    const url = new URL("https://mbodm-wowcam.deno.dev/scrape");
-    url.searchParams.append("token", "d19f023f-bfe0-437a-9daf-7ef28386ebe2");
-    const response = await fetch(url);
-    if (!response.ok) {
-      helper.log(`Requesting '/scrape' in Deno CRON job failed (response status code was HTTP ${response.status}).`);
-    }
-  });
   server.start();
   helper.log("Started Deno API server.");
 }
